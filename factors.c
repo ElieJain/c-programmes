b8b5ee50109395d1de9ec6d54d82878133ab998a
ElieJain <86092920+ElieJain@users.noreply.github.com>
2021-06-18 23:30:46 +0530

    Create factors.c

---
+++ factors.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int x, i; 
+ printf("Please enter a positive number: ");
+scanf("%d", &x); 
+printf("Factors of %d are: ", x); 
+for (i=1; i <= x; ++i){ 
+    if (x % i== 0) { 
+        printf("%d, ", i); 
+    }
+}
+    return 0;
+}
